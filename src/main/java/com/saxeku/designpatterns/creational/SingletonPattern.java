a360fafb22bd1c0db530977a10f76a8a5b49ebc3
Kunal Saxena <kunal.saxena.kunal@gmail.com>
2023-02-21 20:45:34 +0530

    Adding Creational Design Patterns

---
+++ src/main/java/com/saxeku/designpatterns/creational/SingletonPattern.java
@@ -0,0 +1,17 @@
+package com.saxeku.designpatterns.creational;
+
+public final class SingletonPattern { // final preventing sub classing
+
+	private static SingletonPattern singletonPattern;
+	
+	private SingletonPattern() {	// restricting use of new keyword
+	}
+	
+	public static synchronized SingletonPattern getInstance() { // lazy Initialization
+		if(singletonPattern == null) {
+			singletonPattern = new SingletonPattern();
+		}
+		return singletonPattern;
+	}
+	
+}
